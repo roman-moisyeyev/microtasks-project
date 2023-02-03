6c5dffec4dc90da06d9641a6ea3dc846175402b1
Roman Moisyeyev <roman.moisyeyev@gmail.com>
2023-02-03 12:31:00 +0200

    stupid button

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,30 @@ import {Header} from "./site/Header";
 import {Body} from "./site/Body";
 import {Footer} from "./site/Footer";
 import {NewComponent} from "./site/NewComponent";
+import {Button} from "./components/Button";
 
 
 function App() {
- const onClickHandler=(name:string)=>{
-
-     console.log (name)
- }
-    const myFirstSubscriber=(e:React.MouseEvent<HTMLButtonElement>)=>{
-        console.log("hello i am Vasiliy")
+    const Button1Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
     }
-    const mySecondSubscriber=(e:React.MouseEvent<HTMLButtonElement>)=>{
-        console.log("hello i am IVAN")
+    const Button2Foo = (subscriber: string) => {
+        console.log(subscriber)
+    }
+
+    const Button3Foo = () => {
+        console.log("hello, I'm stupid button ")
     }
 
 
     return (
         <div className='App'>
 
-        <button onClick={(event)=>onClickHandler("VASYA")}>MyYoutubeChannel-1</button>
-        <button onClick={(event)=>onClickHandler("NIKOLAS")}>MyYoutubeChannel-2</button>
+            {/*<button>MyYoutubeChannel-1</button>*/}
+            {/*<button>MyYoutubeChannel-2</button>*/}
+            < Button name={"YoutubeChannel_1"} callBack={() => Button1Foo('i am Vasya', 21)}/>
+            < Button name={"YoutubeChannel_2"} callBack={() => Button2Foo('i am Ivan')}/>
+            < Button name={"stupid button"} callBack={Button3Foo}/>
 
 
         </div>
