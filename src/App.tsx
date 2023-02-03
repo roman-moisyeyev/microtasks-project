c13b406d2e80ad2ba44ae7c02ec3e1be2c8d2d64
Roman Moisyeyev <roman.moisyeyev@gmail.com>
2023-02-03 15:14:00 +0200

    The filter() method

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,64 @@ import {Footer} from "./site/Footer";
 import {NewComponent} from "./site/NewComponent";
 import {Button} from "./components/Button";
 
-
+type FilterType= 'all'|'RUBLS'|'dollar'
 function App() {
-let [a,setA]=useState(1)
+    const [money, setMoney] = useState([
+        {banknots: ' Dollars ', value: 100, number: ' a1234567890'},
+        {banknots: ' Dollars ', value: 50, number: ' z1234567890'},
+        {banknots: ' RUBLS ', value: 100, number: ' w1234567890'},
+        {banknots: ' Dollars ', value: 100, number: ' e1234567890'},
+        {banknots: ' Dollars ', value: 50, number: ' c1234567890'},
+        {banknots: ' RUBLS ', value: 100, number: ' r1234567890'},
+        {banknots: ' Dollars ', value: 50, number: ' x1234567890'},
+        {banknots: ' RUBLS ', value: 50, number: ' v1234567890'},
+    ])
 
-    const onclickHandler = ()=>{
-    setA(++a)
+    let currentMoney = money
 
-        console.log(a)
+    const [filter,setFilter] = useState<FilterType>("all")
+
+
+    if (filter === 'dollar') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === ' Dollars ')
+
+    }
+    if (filter === 'RUBLS') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === ' RUBLS ')
+
     }
 
-    const onclickNullHandler =()=>{
-   setA(a=0)
+
+    const onClickFilterHandler = (nameButton: FilterType) => {
+        console.log(nameButton)
+        setFilter(nameButton)
+
+
     }
     return (
-        <div className='App'>
+        <>
+            <ul>
+                {currentMoney.map((objFromMoney, index) => {
+                    return (
 
-<h1>{a}</h1>
-            <button onClick={onclickHandler}>number</button>
-            <button onClick={onclickNullHandler}>0</button>
-        </div>
+                        <li key={index}>
+                            <span>{objFromMoney.banknots}</span>
+                            <span>{objFromMoney.value}</span>
+                            <span>{objFromMoney.number}</span>
+
+
+                        </li>
+                    )
+
+                })}
+
+            </ul>
+            <div style={{marginLeft: '35px'}}>
+                <button onClick={() => onClickFilterHandler('all')}>all</button>
+                <button onClick={() => onClickFilterHandler('RUBLS')}>RUBLS</button>
+                <button onClick={() => onClickFilterHandler('dollar')}>dollar</button>
+            </div>
+        </>
     );
 }
 
