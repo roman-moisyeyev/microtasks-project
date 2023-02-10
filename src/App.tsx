04084f860762681d2337aed562bf7a5a21e56f0e
Roman Moisyeyev <roman.moisyeyev@gmail.com>
2023-02-10 14:38:22 +0200

    input+button as integrated whole

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,41 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Header} from "./site/Header";
-import {Body} from "./site/Body";
-import {Footer} from "./site/Footer";
-import {NewComponent} from "./site/NewComponent";
-import {Button} from "./components/Button";
 
-type FilterType= 'all'|'RUBLS'|'dollar'
+import './App.css';
+import {FullInput} from "./components/FullInput";
+
+
+
+
 function App() {
-    const [money, setMoney] = useState([
-        {banknots: ' Dollars ', value: 100, number: ' a1234567890'},
-        {banknots: ' Dollars ', value: 50, number: ' z1234567890'},
-        {banknots: ' RUBLS ', value: 100, number: ' w1234567890'},
-        {banknots: ' Dollars ', value: 100, number: ' e1234567890'},
-        {banknots: ' Dollars ', value: 50, number: ' c1234567890'},
-        {banknots: ' RUBLS ', value: 100, number: ' r1234567890'},
-        {banknots: ' Dollars ', value: 50, number: ' x1234567890'},
-        {banknots: ' RUBLS ', value: 50, number: ' v1234567890'},
+    let[message,setMessage]=useState([
+        {message:'message1'},
+        {message:'message2'},
+        {message:'message3'}
     ])
 
-    let currentMoney = money
+    const addMessage=(title:string)=>{
+        let newMessage= {message:title}
+        setMessage([newMessage,...message])
 
-    const [filter,setFilter] = useState<FilterType>("all")
-
-
-    if (filter === 'dollar') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === ' Dollars ')
-
-    }
-    if (filter === 'RUBLS') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === ' RUBLS ')
-
+    //
     }
 
-
-    const onClickFilterHandler = (nameButton: FilterType) => {
-        console.log(nameButton)
-        setFilter(nameButton)
-
-
-    }
     return (
-        <>
-            <ul>
-                {currentMoney.map((objFromMoney, index) => {
-                    return (
+        <div className={"App"}>
+            {/*<div>*/}
+            {/*    <input/>*/}
+            {/*    <button>+</button>*/}
+            {/*</div>*/}
+            <FullInput addMessage={addMessage}/>
+            { message.map((el,index)=>{
+                return(
+                    <div key={index}>{el.message}</div>
+                )
 
-                        <li key={index}>
-                            <span>{objFromMoney.banknots}</span>
-                            <span>{objFromMoney.value}</span>
-                            <span>{objFromMoney.number}</span>
+            })
 
-
-                        </li>
-                    )
-
-                })}
-
-            </ul>
-            <div style={{marginLeft: '35px'}}>
-                <button onClick={() => onClickFilterHandler('all')}>all</button>
-                <button onClick={() => onClickFilterHandler('RUBLS')}>RUBLS</button>
-                <button onClick={() => onClickFilterHandler('dollar')}>dollar</button>
-            </div>
-        </>
+            }
+        </div>
     );
 }
 
---
+++ src/components/FullInput.tsx
@@ -0,0 +1,30 @@
+import React, {ChangeEvent, useState} from "react";
+
+type FullInputType ={
+    addMessage:(title:string)=>void
+}
+
+export const FullInput=(props:FullInputType)=>{
+    let [title,setTitle]=useState('')
+
+    const onChangeInputHandler=(event:ChangeEvent<HTMLInputElement>)=>{
+        setTitle(event.currentTarget.value)
+
+
+    }
+    const onClickButtonHandler =(event:React.MouseEvent<HTMLButtonElement>)=>{
+        props.addMessage(title)
+        setTitle('')
+
+
+
+    }
+
+
+    return(
+        <div>
+            <input value={title} onChange={onChangeInputHandler}/>
+            <button onClick={onClickButtonHandler}>+</button>
+        </div>
+    )
+ }
